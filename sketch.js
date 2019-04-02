360429879d28c2ba4c353a18f59ac6643fdae644
John Underhill <junderhill@qisproject.com>
2019-04-02 16:34:25 -0700

    Make some changes

--- sketch.js
+++ sketch.js
@@ -1,17 +1,19 @@
 currentx = 100;
 currenty = 100;
 
+CIRCLESIZE = 100;
+
 function setup() {
   createCanvas(1000, 1000);
   fill(0);
-  ellipse(currentx, currenty, 80, 80);
+  ellipse(currentx, currenty, CIRCLESIZE, CIRCLESIZE);
 }
 
 function draw() {
   fill(255);
-  ellipse(currentx, currenty, 80, 80);
+  ellipse(currentx, currenty, CIRCLESIZE, CIRCLESIZE);
   currentx = mouseX;
   currenty = mouseY;
   fill(0);  
-  ellipse(currentx, currenty, 80, 80);
+  ellipse(currentx, currenty, CIRCLESIZE, CIRCLESIZE);
 }
